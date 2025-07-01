f6612a4f6f1e555ccdaee0dbffca9109e9b9926f
Mayaqq <i@mayaqq.dev>
2025-07-01 23:09:56 +0200

    started work on kotlin datagen

--- build.gradle.kts
+++ build.gradle.kts
@@ -191,6 +191,38 @@ cloche {
         }
     }
 
+    fabric("datagen") {
+        loaderVersion = libs.versions.fabric
+        minecraftVersion = libs.versions.minecraft
+
+        dependencies {
+            fabricApi(libs.versions.fapi)
+            modApi(libs.fabric.kotlin)
+            modApi.bundle(libs.bundles.fabric.cardinalComponents)
+            modImplementation(libs.fabric.baubly) { exclude(group = "me.shedaniel") }
+            modImplementation(libs.fabric.trinkets)
+            modCompileOnly(libs.fabric.emi)
+            modCompileOnly(libs.fabric.rei)
+            modCompileOnly(libs.fabric.jei)
+            modImplementation(libs.fabric.modmenu)
+            modCompileOnly(libs.fabric.iris)
+            modCompileOnlyApi(libs.fabric.flywheel.api)
+            modImplementation(libs.fabric.flywheel)
+            modApi(libs.fabric.cynosure)
+            modApi(libs.fabric.kritter)
+            modApi(libs.fabric.kittyconfig)
+        }
+
+        data {
+
+        }
+
+        runs {
+            data()
+            clientData()
+        }
+    }
+
     forge {
         mixins.from(file("src/main/estrogen.mixins.json"))
         accessWideners.from(file("src/main/estrogen.accessWidener"))
